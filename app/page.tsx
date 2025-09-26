0363a59fe0c00a16df842fd1be8f802ef09ecb1b
Minh Tran <trcongminh.261002@gmail.com>
2025-09-26 11:17:32 +0700

    Update layout and Home component for enhanced branding and features
    - Updated metadata in layout.tsx to reflect new branding for "Sorana AI - Transform Your Meetings with AI Magic".
    - Reintroduced globals.css for consistent styling.
    - Added FeaturesSection component to Home for improved user engagement.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-
+import FeaturesSection from "./components/landing/FeatureSection";
 import HeroSection from "./components/landing/HeroSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
       <HeroSection />
+      <FeaturesSection />
+      {/* <IntegrationsSection />
+      <HowItWorksSection />
+      <StatsSection />
+      <MoreFeaturesSection />
+      <CTASection />
+      <Footer /> */}
     </div>
   );
 }
