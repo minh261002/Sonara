e0a38a3f3efa447f1e9dc5c5a0e4d98f7a3fb8f1
Minh Tran <trcongminh.261002@gmail.com>
2025-09-26 11:20:08 +0700

    Enhance Home component and remove unused SVG files
    - Added IntegrationsSection component to the Home layout for improved functionality.
    - Removed several unused SVG files from the public directory to clean up the project.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import FeaturesSection from "./components/landing/FeatureSection";
 import HeroSection from "./components/landing/HeroSection";
+import IntegrationsSection from "./components/landing/IntegrationsSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
       <HeroSection />
       <FeaturesSection />
-      {/* <IntegrationsSection />
-      <HowItWorksSection />
+      <IntegrationsSection />
+      {/* <HowItWorksSection />
       <StatsSection />
       <MoreFeaturesSection />
       <CTASection />
